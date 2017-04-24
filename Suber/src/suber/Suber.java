b5454d63b1a6e7c83d28595933766c79b81b8e9d
Andrew <z5112661@student.unsw.edu.au>
2017-04-24 18:28:44 +1000

    Create foundations for Suber

---
+++ Suber/src/suber/Suber.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suber;
+
+/**
+ *
+ * @author Andrew
+ */
+public class Suber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
