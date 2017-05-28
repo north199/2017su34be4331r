0d7c837a6db42bfc60cf9c6b0a6783a407720b39
michaelliangaustralia <michaelliang15@gmail.com>
2017-05-28 21:28:32 +1000

    Add files via upload
    updated to excluded time and include estimated traveltime.

--- PaymentsAlgorithm.java
+++ PaymentsAlgorithm.java
@@ -1,6 +1,7 @@
 package suber.infs2605;
 
 import java.sql.Timestamp;
+
 public class PaymentsAlgorithm {
 
     /* Cost = fuel cost + 5 (for time to pick up)
@@ -9,26 +10,29 @@ Fuel cost = Pull database data on end and start time then mulitply it by 3
     Fields are cost, start time, end time, multiplier (constant), fixed fee
      */
     private double cost;
-    public final int fuelconstant = 3;
-    public final int fixedfee = 5;
-    public Timestamp starttime;
-    public Timestamp endtime;
+    private final int fuelconstant = 3;
+    private final int fixedfee = 5;
+    private int estimatedtraveltime;
+//    public Timestamp starttime;
+//    public Timestamp endtime;
 
-    public PaymentsAlgorithm(){
+    public PaymentsAlgorithm() {
         cost = 0;
-        starttime = new Timestamp(0,0,0,0,0,0,0);
-        endtime = new Timestamp(0,0,0,0,0,0,0);
-        
+        estimatedtraveltime = 0;
+//        starttime = new Timestamp(0,0,0,0,0,0,0);
+//        endtime = new Timestamp(0,0,0,0,0,0,0);
+
     }
-    
-        public PaymentsAlgorithm(Timestamp starttime, Timestamp endtime){
-        starttime = this.starttime;
-        endtime = this.endtime;
-        
+
+    public PaymentsAlgorithm(int estimatedtraveltime) {
+        estimatedtraveltime = this.estimatedtraveltime;
+//        starttime = this.starttime;
+//        endtime = this.endtime;
+
     }
-    
+
     public double calculateCost(Timestamp starttime, Timestamp endtime) {
-        cost = (this.endtime.getTime() - this.starttime.getTime()) * fuelconstant + fixedfee;
+        cost = estimatedtraveltime * fuelconstant + fixedfee;
         return cost;
     }
 
@@ -48,23 +52,11 @@ Fuel cost = Pull database data on end and start time then mulitply it by 3
         return fixedfee;
     }
 
-
-    public Timestamp getStarttime() {
-        return starttime;
+    public int getEstimatedtraveltime() {
+        return estimatedtraveltime;
     }
 
-    public void setStarttime(Timestamp starttime) {
-        this.starttime = starttime;
+    public void setEstimatedtraveltime(int estimatedtraveltime) {
+        this.estimatedtraveltime = estimatedtraveltime;
     }
-
-    public Timestamp getEndtime() {
-        return endtime;
-    }
-
-    public void setEndtime(Timestamp endtime) {
-        this.endtime = endtime;
-    }
-
-    
-    
 }
